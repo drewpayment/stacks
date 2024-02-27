e0317ae3e8ad80ed43282d74811cd33a20c9580a
drewpayment <drew.payment@gmail.com>
2024-02-26 22:42:36 -0500

    🎨 style(paystubs): improve layout and add breadcrumb navigation 🔧 chore(paystubs): remove unused import 📝 docs(paystubs): add description for paystubs page

--- src/routes/(app)/app/paystubs/+page.svelte
+++ src/routes/(app)/app/paystubs/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+
   
   export let data;
   
   console.dir(data.paystubs);
 </script>
 
-<p>view paystubs page</p>
+<div class="container max-w-5xl">
+  <div class="flex">
+    <div>
+      <h4 class="mb-2">My Pay</h4>
+      <p class="mb-4">
+        View my pay information.
+      </p>
+    </div>
+  </div>
+  
+  <div class="pb-4">
+    <Breadcrumb>
+      <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+      <BreadcrumbItem>My Paystubs</BreadcrumbItem>
+    </Breadcrumb>
+  </div>
+  
+  {JSON.stringify(data.paystubs)}
+</div>
