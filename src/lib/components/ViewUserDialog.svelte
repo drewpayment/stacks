35703f2b3a2f1dcabc87abb0d7fe4f3955ada73c
drewpayment <drew.payment@gmail.com>
2024-03-12 23:03:06 -0400

    feat (server.ts): introduce process.env.PORT environment variable for running app on configurable port refactor (server.ts): change port variable case from lowercase to uppercase for better semantics feat (ViewUserDialog.svelte): revamp user role selection implementation using flowbite-svelte feat (ViewUserDialog.svelte): revamp user profile form using flowbite-svelte

--- src/lib/components/ViewUserDialog.svelte
+++ src/lib/components/ViewUserDialog.svelte
@@ -2,14 +2,12 @@
   import { enhance } from '$app/forms';
   import SelectedClientStore from '$lib/stores/client';
 	import type { User } from '$lib/types/db.model';
-  import Icon from '@iconify/svelte';
-  import { createDialog, melt, createLabel, createSelect, createToaster, type CreateDialogProps } from '@melt-ui/svelte';
-	import { Check, ChevronDown, Eye, XSquare } from 'lucide-svelte';
-  import type { Unsubscriber } from 'svelte/store';
-	import { createToast } from './Toast.svelte';
+  import { createDialog, melt, createLabel, createSelect } from '@melt-ui/svelte';
+	import { Check, ChevronDown } from 'lucide-svelte';
+  import { createToast } from './Toast.svelte';
 	import UserStore from '$lib/stores/user';
-	import { Button, CloseButton } from 'flowbite-svelte';
-	import { EyeSolid } from 'flowbite-svelte-icons';
+	import { Button, CloseButton, Input, Label, Select } from 'flowbite-svelte';
+	import { EnvelopeSolid, EyeSolid } from 'flowbite-svelte-icons';
   
   export let user: User;
   let origUser: User;
@@ -37,10 +35,13 @@
   } = createLabel();
   
   const options = [
-    { label: 'Admin', value: 'admin' },
-    { label: 'Manager', value: 'manager' },
-    { label: 'User', value: 'user' },
+    { name: 'Admin', value: 'admin' },
+    { name: 'Manager', value: 'manager' },
+    { name: 'User', value: 'user' },
   ];
+  
+  let userRole = user.user_profile.role;
+  $: selectedUserRole = options.find(x => x.value === userRole);
   
   if (user) {
     origUser = {...user};
@@ -122,8 +123,7 @@
               UserStore.update(users => {
                 const user = users.findIndex(x => x.auth_user.id === payload.user_id &&
                   x.user_profile.id === payload.user_profile_id);
-                console.log(payload);
-                
+                  
                 if (user > -1) {
                   for (const prop in payload) {
                     if (prop === 'user_id' || prop === 'user_profile_id') continue;
@@ -157,117 +157,45 @@
           };
         }}
       >
-        <div class="flex flex-col items-start justify-center">
-          <div class="flex flex-col items-start justify-center pb-2">
-            <label use:melt={$root} for="first_name" class="mb-0.5 font-medium text-neutral-900 dark:text-neutral-50" data-melt-part="root">
-              First Name
-            </label>
+        <div class="flex gap-2 mb-2">
+          <div class="space-y-2">
+            <Label for="first_name">First Name</Label>
             <input type="hidden" name="user_id" value={user.auth_user.id} />
             <input type="hidden" name="user_profile_id" value={user.user_profile.id} />
-            <input type="text" name="first_name" id="first_name" required class="h-10 w-[240px] rounded-md px-3 py-2 text-neutral-700 dark:bg-gray-700 dark:text-neutral-50"
-              value={user.user_profile.firstName}
-            />
+            <Input type="text" name="first_name" id="first_name" value={user.user_profile.firstName} required />
           </div>
           
-          <div class="flex flex-col items-start justify-center pb-2">
-            <label use:melt={$root} for="last_name" class="mb-0.5 font-medium text-neutral-900 dark:text-neutral-50" data-melt-part="root">
-              Last Name
-            </label>
-            <input type="text" name="last_name" id="last_name" required class="h-10 w-[240px] rounded-md px-3 py-2 text-neutral-700 dark:bg-gray-700 dark:text-neutral-50" 
-              value={user.user_profile.lastName}
-            />
-          </div>
-          
-          <div class="flex flex-col items-start justify-center pb-2">
-            <label use:melt={$root} for="email" class="mb-0.5 font-medium text-neutral-900 dark:text-neutral-50" data-melt-part="root">
-              Email
-            </label>
-            <input type="email" name="email" id="email" required class="h-10 w-[240px] rounded-md px-3 py-2 text-neutral-700 dark:bg-gray-700 dark:text-neutral-50" 
-              value={user.auth_user.email}
-            />
-          </div>
-          
-          <div class="pb-2">
-            <button 
-              use:melt={$triggerSelect} 
-              class="flex h-10 min-w-[220px] items-center justify-between rounded-lg bg-white px-3 py-2 shadow transition-opacity hover:opacity-90 dark:bg-gray-700 dark:text-neutral-50"
-              on:m-keydown={(e) => {
-                e.preventDefault(); // Cancel default builder behabiour
-                e.detail.originalEvent.preventDefault(); // Cancel page scroll
-           
-                const { key } = e.detail.originalEvent;
-           
-                if (!['ArrowDown', 'ArrowUp', 'Space', 'Enter'].includes(key)) return;
-           
-                const index = options.findIndex(x => x.label === `${$selectedLabel}`);
-           
-                if (key === 'ArrowDown') {
-                  const nextIndex = index + 1;
-                  const nextOption = options[nextIndex] || options[0];
-                  selected.set(nextOption);
-                } else if (key === 'ArrowUp') {
-                  const prevIndex = index - 1;
-                  const prevOption = options[prevIndex] || options[options.length - 1];
-                  selected.set(prevOption);
-                } else {
-                  open.set(true);
-                }
-              }}
-            >
-              <span>{$selectedLabel || 'Select a Role'}</span>
-              <ChevronDown class="square-5" />
-            </button>
-            
-            {#if $openSelect}
-              <div class="z-10 flex max-h-[300px] flex-col overflow-y-auto rounded-lg bg-white p-1 shadow focus:!ring-0 dark:bg-gray-700"
-                use:melt={$menu}
-              >
-                {#each options as item}
-                <div
-                  class="relative cursor-pointer rounded-lg py-1 pl-8 pr-4 text-neutral-800 dark:text-neutral-50
-                  focus:z-10 focus:text-neutral-700 dark:focus:text-neutral-100
-                data-[highlighted]:bg-magnum-50 data-[selected]:bg-neutral-100 dark:data-[highlighted]:bg-neutral-700 dark:data-[selected]:bg-neutral-700
-                data-[highlighted]:text-neutral-900 data-[selected]:text-neutral-900 dark:data-[highlighted]:text-neutral-50 dark:data-[selected]:text-neutral-50"
-                  use:melt={$option(item)}
-                  on:m-click={(e) => {
-                    e.preventDefault();
-                    
-                    selected.set(item);
-                    openSelect.set(false);
-                  }}
-                >
-                  <div class="check {$isSelected(item) ? 'block' : 'hidden'}">
-                    <Check class="square-4" />
-                  </div>
-    
-                  {item.label}
-                </div>
-                {/each}
-              </div>
-            {/if}
-          </div>
-          
-          <div class="flex flex-col items-start justify-center pb-2">
-            <label for="client_id"
-              use:melt={$root}
-              class="mb-0.5 font-medium text-neutral-900 dark:text-neutral-50"
-              data-melt-part="root"
-            >Client</label>
-            <input type="text" name="client_id" id="client_id" readonly
-              placeholder={$SelectedClientStore}
-              value={$SelectedClientStore}
-              class="h-10 w-[240px] rounded-md px-3 py-2 text-neutral-700 dark:text-neutral-100 dark:bg-gray-700" />
+          <div class="space-y-2">
+            <Label for="last_name">Last Name</Label>
+            <Input type="text" name="last_name" id="last_name" value={user.user_profile.lastName} required />
           </div>
         </div>
         
-        <div class="flex justify-between">
-          <button type="button" use:melt={$close}>
-            Cancel
-          </button>
+        <div class="space-y-2 mb-2">
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" value={user.auth_user.email} required>
+            <EnvelopeSolid slot="left" class="w-4 h-4" />
+          </Input>
+        </div>
+        
+        <div class="mb-2">
+          <Label>
+            User Type
+            <Select class="mt-2" items={options} bind:value={userRole} placeholder="Choose user type" name="" required />
+          </Label>
+        </div>
+        
+        <div class="space-y-2 mb-2">
+          <Label>Client</Label>
+          <Input type="text" name="client_id" id="client_id" bind:value={$SelectedClientStore} disabled />
+        </div>
+        
+        <div class="flex justify-between pt-4">
+          <span use:melt={$close}>
+            <Button color="alternative">Cancel</Button>
+          </span>
           
-          <button type="submit">
-            Save
-          </button>
+          <Button type="submit">Save</Button>
         </div>
       </form>
     </div>
