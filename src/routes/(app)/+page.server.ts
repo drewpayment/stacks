c8a01d8822d1013226a1924a21864e7cb59ff73d
drewpayment <drew.payment@gmail.com>
2024-08-24 10:05:11 -0400

    feat(routes/(app)/+page.server.ts): add support for fetching all payroll cycles
    This commit adds support for fetching all payroll cycles for a given client. This allows users to view a history of payroll cycles and their associated paystubs.
    
    feat(app): add payroll cycles history table
    
    This commit adds a table to the app page that displays the history of payroll cycles. The table includes the following columns:
    
    - Paid Date
    - Started
    - End
    - No of Paystubs
    - Closed
    
    The table is only visible to users with the `org_admin`, `super_admin`, or `admin` role.

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,28 +1,49 @@
-import type { SelectEmployee, SelectPayrollCycle, SelectPaystub } from '$lib/drizzle/postgres/db.model';
-import { getLastPayrollCycle } from '$lib/drizzle/postgres/models/payroll-cycles';
+import type {
+	SelectEmployee,
+	SelectPayrollCycle,
+	SelectPaystub
+} from '$lib/drizzle/postgres/db.model';
+import { getLastPayrollCycle, getPayrollCycles } from '$lib/drizzle/postgres/models/payroll-cycles';
 import { UserClient } from '$lib/drizzle/postgres/models/user-clients';
 import type { PageServerLoad } from './$types';
-
+import { drizzleClient as db } from '$lib/drizzle/postgres/client';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  if (!locals.user || !locals.user.profile) {
-    return {};
-  }
-  
-  const profile = locals.user?.profile;
-  
-  if (!profile || !profile.clientId) return {};
-  const clientId = profile?.clientId as string;
-  const userId = locals.user.id;
-  
-  if (!(await UserClient.canAccess(userId, clientId))) return {};
-  
-  // get latest payroll cycle with associated paystubs 
-  const getLatestPayrollCycle = async (): Promise<SelectPayrollCycle & { paystubs: (SelectPaystub & { employee: SelectEmployee })[] }> => {
-    return (await getLastPayrollCycle(clientId)) as SelectPayrollCycle & { paystubs: (SelectPaystub & { employee: SelectEmployee })[] };
-  }
-  
-  return {
-    lastPayrollCycle: await getLatestPayrollCycle(),    
-  };
+	if (!locals.user || !locals.user.profile) {
+		return {};
+	}
+
+	const profile = locals.user?.profile;
+
+	if (!profile || !profile.clientId) return {};
+	const clientId = profile?.clientId as string;
+	const userId = locals.user.id;
+
+	if (!(await UserClient.canAccess(userId, clientId))) return {};
+
+	// get latest payroll cycle with associated paystubs
+	const getLatestPayrollCycle = async (): Promise<
+		SelectPayrollCycle & { paystubs: (SelectPaystub & { employee: SelectEmployee })[] }
+	> => {
+		return (await getLastPayrollCycle(clientId)) as SelectPayrollCycle & {
+			paystubs: (SelectPaystub & { employee: SelectEmployee })[];
+		};
+	};
+
+	const payrollCycles = async () => {
+		const data = await db.query.payrollCycle.findMany({
+			with: {
+				paystubs: true
+			},
+			where: (pc, { eq }) => eq(pc.clientId, clientId),
+			orderBy: (pc, { desc }) => [desc(pc.paymentDate)]
+		});
+
+		return data as (SelectPayrollCycle & { paystubs: SelectPaystub[] })[];
+	};
+
+	return {
+		lastPayrollCycle: await getLatestPayrollCycle(),
+		cycles: await payrollCycles()
+	};
 };
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	import type { SelectEmployee, SelectPayrollCycle, SelectPaystub } from '$lib/drizzle/postgres/db.model';
+	import Toast from '$lib/components/Toast.svelte';
+	import type {
+		SelectEmployee,
+		SelectPayrollCycle,
+		SelectPaystub
+	} from '$lib/drizzle/postgres/db.model';
 	import { formatCurrency, toHumanDate } from '$lib/utils/utils';
-	import { Button, Card } from 'flowbite-svelte';
+	import {
+		Button,
+		Card,
+		Checkbox,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
 	import { CheckCircleSolid } from 'flowbite-svelte-icons';
-
+	import { Icon, ArrowUpRight } from 'svelte-hero-icons';
 	export let data;
-	
-	const { lastPayrollCycle, profile, user } = data;
-	
-	const cycle = (lastPayrollCycle || {}) as SelectPayrollCycle & { paystubs: (SelectPaystub & { employee: SelectEmployee })[] };
+	const { lastPayrollCycle, profile, user, cycles: historyCycles } = data;
+	const cycle = (lastPayrollCycle || {}) as SelectPayrollCycle & {
+		paystubs: (SelectPaystub & { employee: SelectEmployee })[];
+	};
+	const cycles = historyCycles as (SelectPayrollCycle & { paystubs: SelectPaystub[] })[];
 </script>
 
 <svelte:head>
@@ -19,56 +35,95 @@
 	{#if user}
 		<h4>Welcome, {profile?.firstName} {profile?.lastName}.</h4>
 	{/if}
-
-	{#if user && ['org_admin', 'super_admin'].includes(user.profile.role)}
-	<div
-		class="flex flex-col gap-2 mt-4 px-1 py-6 bg-secondary-100 border-secondary-100
+	{#if user && ['org_admin', 'super_admin', 'admin'].includes(user.profile.role)}
+		<div
+			class="flex flex-col gap-2 mt-4 px-1 py-6 bg-secondary-100 border-secondary-100
 		dark:bg-background-300 border dark:border-background-300 rounded-md shadow-md"
-	>
-		<div class="px-4 mb-4 text-2xl font-medium">Latest Payroll</div>
-		<div class="flex flex-col md:flex-row md:justify-around">
-			{#if user && ['org_admin', 'super_admin'].includes(user.profile.role)}
-			<Card size="sm">
-				<h5 class="mb-4 text-2xl font-medium text-gray-600 dark:text-gray-50">{ toHumanDate(cycle.paymentDate) }</h5>
-				<div class="flex items-baseline text-gray-900 dark:text-white">
-					<ul class="my-7 space-y-4">
-						<li class="flex space-x-2 rtl:space-x-reverse">
-							<CheckCircleSolid class="w-4 h-4 text-primary-600 dark:text-primary-500" />
-							<span class="text-xl font-extrabold tracking-tight">{ cycle.paystubs?.length } paystubs</span>
-						</li>
-						<li class="flex space-x-2 rtl:space-x-reverse">
-							<CheckCircleSolid class="w-4 h-4 text-primary-600 dark:text-primary-500" />
-							<span class="text-xl font-extrabold tracking-tight">${ cycle.paystubs?.reduce((acc, curr) => acc + Number(curr.netPay), 0) }</span> total net
-						</li>
-					</ul>
-				</div>
-				<div class="flex flex-col w-full md:flex-row md:justify-end md:w-auto">
-					<Button href={'/app/payroll-cycles/' + cycle.id}>View Details</Button>
-				</div>
-			</Card>
-			
-			<Card size="sm">
-				<div class="mb-4 text-xl font-medium text-gray-400 dark:text-gray-500">Top Performers</div>
-				<!-- <h5 class="mb-4 text-2xl font-medium text-gray-600 dark:text-gray-50">Sales</h5> -->
-				<div class="flex items-baseline text-gray-900 dark:text-white">
-					{#each cycle?.paystubs as paystub (paystub.id)}
-						<div class="flex flex-col md:flex-row justify-between gap-2 w-full">
-							<span class="text-xl font-extrabold tracking-tight">{ paystub.employee.firstName } { paystub.employee.lastName }</span>
-							<span class="text-lg font-bold tracking-tight">{ formatCurrency(paystub.netPay) }</span>
+		>
+			<div class="px-4 mb-4 text-2xl font-medium">Latest Payroll</div>
+			<div class="flex flex-col md:flex-row md:justify-around">
+				{#if user && ['org_admin', 'super_admin'].includes(user.profile.role)}
+					<Card size="sm">
+						<h5 class="mb-4 text-2xl font-medium text-gray-600 dark:text-gray-50">
+							{toHumanDate(cycle.paymentDate)}
+						</h5>
+						<div class="flex items-baseline text-gray-900 dark:text-white">
+							<ul class="my-7 space-y-4">
+								<li class="flex space-x-2 rtl:space-x-reverse">
+									<CheckCircleSolid class="w-4 h-4 text-primary-600 dark:text-primary-500" />
+									<span class="text-xl font-extrabold tracking-tight"
+										>{cycle.paystubs?.length} paystubs</span
+									>
+								</li>
+								<li class="flex space-x-2 rtl:space-x-reverse">
+									<CheckCircleSolid class="w-4 h-4 text-primary-600 dark:text-primary-500" />
+									<span class="text-xl font-extrabold tracking-tight"
+										>${cycle.paystubs?.reduce((acc, curr) => acc + Number(curr.netPay), 0)}</span
+									> total net
+								</li>
+							</ul>
 						</div>
-					{/each}
-				</div>
-			</Card>
-		{:else}
-			<h5 class="dark:text-text-600">What is Stacks?</h5>
-			<p class="leading-9">Please log in to get started.</p>
-		{/if}
+						<div class="flex flex-col w-full md:flex-row md:justify-end md:w-auto">
+							<Button href={'/app/payroll-cycles/' + cycle.id}>View Details</Button>
+						</div>
+					</Card>
+					<Card size="sm">
+						<div class="mb-4 text-xl font-medium text-gray-400 dark:text-gray-500">
+							Top Performers
+						</div>
+						<div class="flex items-baseline text-gray-900 dark:text-white">
+							{#each cycle?.paystubs as paystub (paystub.id)}
+								<div class="flex flex-col md:flex-row justify-between gap-2 w-full">
+									<span class="text-xl font-extrabold tracking-tight"
+										>{paystub.employee.firstName} {paystub.employee.lastName}</span
+									>
+									<span class="text-lg font-bold tracking-tight"
+										>{formatCurrency(paystub.netPay)}</span
+									>
+								</div>
+							{/each}
+						</div>
+					</Card>
+				{:else}
+					<h5 class="dark:text-text-600">What is Stacks?</h5>
+					<p class="leading-9">Please log in to get started.</p>
+				{/if}
+			</div>
 		</div>
-		
-		<!-- <div class="flex justify-center w-full p-4">
-			<Button href={'/app/payroll-cycles/' + cycle.id}>View Details</Button>
-		</div> -->
-	</div>
-	
+		<div
+			class="flex flex-col gap-2 mt-4 px-1 py-6 bg-secondary-100 border-secondary-100 dark:bg-background-300 border dark:border-background-300 rounded-md shadow-md"
+		>
+			<div class="px-4 mb-4 text-2xl font-medium">Payroll Cycles</div>
+			<div class="flex flex-col md:flex-row md:justify-start px-12">
+				<Table striped={true} shadow={true}>
+					<TableHead theadClass="bg-primary-100">
+						<TableHeadCell>&nbsp;</TableHeadCell>
+						<TableHeadCell>Paid Date</TableHeadCell>
+						<TableHeadCell>Started</TableHeadCell>
+						<TableHeadCell>End</TableHeadCell>
+						<TableHeadCell>No of Paystubs</TableHeadCell>
+						<TableHeadCell>Closed</TableHeadCell>
+					</TableHead>
+					<TableBody>
+						{#each cycles as cycle (cycle.id)}
+							<TableBodyRow>
+								<TableBodyCell>
+									<a href={`/app/payroll-cycles/${cycle.id}`} class="w-4 h-4">
+										<Icon src={ArrowUpRight} class="w-4 h-4" />
+									</a>
+								</TableBodyCell>
+								<TableBodyCell>{toHumanDate(cycle.paymentDate)}</TableBodyCell>
+								<TableBodyCell>{toHumanDate(cycle.startDate)}</TableBodyCell>
+								<TableBodyCell>{toHumanDate(cycle.endDate)}</TableBodyCell>
+								<TableBodyCell tdClass="text-center">{cycle.paystubs?.length}</TableBodyCell>
+								<TableBodyCell>
+									<Checkbox checked={cycle.isClosed} disabled="true" />
+								</TableBodyCell>
+							</TableBodyRow>
+						{/each}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
 	{/if}
 </div>
