85f9c18804bc55dae5475330f57df5601a340e19
drewpayment <drew.payment@gmail.com>
2025-03-14 23:32:48 -0400

    refactor(supabase): improve client initialization to handle build environments
    The Supabase client is now initialized conditionally,
    avoiding errors during the build process in production.  A
    dummy client is used during the build, preventing
    unnecessary calls to the Supabase API.

--- src/lib/server/supabase/client.ts
+++ src/lib/server/supabase/client.ts
@@ -1,4 +1,13 @@
 import { SUPABASE_API_KEY, SUPABASE_URL } from '$env/static/private';
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+const isBuild = process.env.NODE_ENV === 'production' && process.env.BUILD_MODE === 'true';
+let supabase: SupabaseClient<any, "public", any>;
+
+if (isBuild) {
+  supabase = {} as any;
+} else {
+  supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+}
+
+export { supabase };
